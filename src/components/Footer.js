c9586f224f9eb7346d0ef0846ae71df6aa8d22b9
Shubham Gupta <shubham@tag-hive.com>
2023-12-22 16:13:36 +0530

    some improvement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,43 @@ const Footer = () => {
     return (
         <React.Fragment>
             <section className="footer" id="contact">
+                <div className="contact">
+
+                    <div className="con">
+                        <i className="fas fa-envelope"></i>
+                        <p>sg350348@gmail.com</p>
+                    </div>
+
+                    <div className="con">
+                        <i className="fas fa-map-marker-alt"></i>
+                        <p>Carrer Developers (Azamgarh)</p>
+                    </div>
+
+                    <div className="con">
+                        <i className="fa fa-phone"></i>
+                        <p>9559364730</p>
+                    </div>
+
+                </div>
+
+                <div className="connect-with-us">
+                    <div className="contact-heading">
+                        <h3>Connect with us</h3>
+                        <p>
+                            24/7 available . Contact with us to get Started. We are available at instagram , facebook , whatsapp and twitter.
+                        </p>
+                    </div>
+
+                    <div className="icons">
+                        <a href="https://www.facebook.com/shubhamgupta9559/"><i className="fab fa-facebook"></i></a>
+                        <a href="https://twitter.com/shubhgupta100/"><i className="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/shubham.gupta100/"><i className="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+
                 <ul className="cards">
                     <li>
-                        <a href="https://shubhamgupta100.github.io/shubham_portfolio/" className="card">
+                        <span className="card">
                             <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
                             <div className="card__overlay">
                                 <div className="card__header">
@@ -22,43 +56,9 @@ const Footer = () => {
                                 <p className="card__description">Shubham is a B.tech graduate  he is a software enginner and
                                     working in this domain since last 2 years.</p>
                             </div>
-                        </a>
+                        </span>
                     </li>
                 </ul>
-
-                <div className="contact">
-
-                    <div className="con">
-                        <i className="fas fa-envelope"></i>
-                        <p>sg350348@gmail.com</p>
-                    </div>
-
-                    <div className="con">
-                        <i className="fas fa-map-marker-alt"></i>
-                        <p>Carrer Developers</p>
-                    </div>
-
-                    <div className="con">
-                        <i className="fa fa-phone"></i>
-                        <p>9559364730</p>
-                    </div>
-
-                </div>
-
-                <div className="connect-with-us">
-                    <div className="contact-heading">
-                        <h3>Connect with us</h3>
-                        <p>
-                            Consume out of the box data Analytics anwhere, anytime. Contact us to get Started.
-                        </p>
-                    </div>
-
-                    <div className="icons">
-                        <a href="https://www.facebook.com/shubhamgupta9559/"><i className="fab fa-facebook"></i></a>
-                        <a href="https://twitter.com/shubhgupta100/"><i className="fab fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/shubham.gupta100/"><i className="fab fa-instagram"></i></a>
-                    </div>
-                </div>
             </section>
         </React.Fragment>
     )
