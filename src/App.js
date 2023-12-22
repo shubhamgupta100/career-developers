a9de0b8cca01578bee3d21c443e4dab95dfa2601
Shubham Gupta <shubham@tag-hive.com>
2023-12-22 10:08:49 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Courses from './components/Courses';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-// import Teachers from './components/Teachers';
+import Teachers from './components/Teachers';
+import { Helmet } from 'react-helmet';
+
+
 
 function App() {
-  return (
+  return (<>
+    <Helmet>
+      <meta property="og:title" content={"Career Developers"} />
+      <meta property="og:description" content={"Azamgarh Best educational coaching center , best coaching in azamgarh , azamgarh"} />
+      {/* <meta property="og:image" content={URL of the image you want to use} /> */}
+      {/* <meta property="og:url" content={URL of your page} /> */}
+      {/* <meta property="og:type" content="website" /> */}
+    </Helmet>
     <div >
-     <Navbar/>
-     <Hero/>
-     <Courses/>
-     {/* <Teachers/> */}
-     <Footer/>
+      <Navbar />
+      <Hero />
+      <Courses />
+      <Teachers/>
+      <Footer />
     </div>
+  </>
   );
 }
 
